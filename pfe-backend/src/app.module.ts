fe32a6d4d2fd33207cf2ce5dfb1d561fa9c51f65
Filipe-Cardoso <71384301+Filipe-Cardoso@users.noreply.github.com>
2021-12-07 11:31:55 +0100

    add connection to db

--- pfe-backend/src/app.module.ts
+++ pfe-backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Mongoose } from 'mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
