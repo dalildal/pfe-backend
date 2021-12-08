08590a4f7a83f36ba7f8fb98a17cd6d2df455a99
Senhaji Obey <obey.senhaji@student.vinci.be>
2021-12-08 14:25:49 +0100

    add feat token for login

--- pfe-backend/src/users/services/userService.ts
+++ pfe-backend/src/users/services/userService.ts
@@ -1,18 +1,41 @@
-import { Body, Injectable } from "@nestjs/common"
-import { create } from "domain"
-import { UserDTO } from "../models/userDto"
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common"
 import { InjectModel } from "@nestjs/mongoose"
 import { Model } from "mongoose"
+import * as bcrypt from 'bcrypt';
+import { User } from "../models/user.interface"
+import { LoginUserDto } from "../models/DTO/LoginUser.dto";
+import { AuthService } from "src/auth/services/authService";
+import { from } from "rxjs/internal/observable/from";
 
 
 @Injectable()
 export class UserService{
+    jwtService: any;
+    
+ 
 
-    constructor(@InjectModel('User') private readonly userModel: Model<UserDTO>) {
+    constructor(
+        @InjectModel('User') 
+        private readonly userModel: Model<User>,
+        private authService: AuthService
+        
+        ) {
         
     }
 
-
+   
+    async verify(loginUser:LoginUserDto) {
+        let user:User = await this.findUserByEmail(loginUser.email);
+        if(!user){
+            throw new HttpException('Login was not Successfull',HttpStatus.UNAUTHORIZED);
+        }
+        const isMatch = await bcrypt.compare(loginUser.password, user.password);
+        console.log(isMatch)
+        if(!isMatch){
+            throw new HttpException('Wrong password or email',HttpStatus.UNAUTHORIZED);
+        }
+        return from(this.authService.generateJwt(user));
+    }
     
     getUser(id: string) {
         this.userModel.findById
@@ -25,10 +48,24 @@ export class UserService{
     }
     
     /*TODO connexion DB*/
-    async create(userDTO: UserDTO){
-        const newUser = new this.userModel(userDTO);
-         let user = await newUser.save();
-       return user.id as string;
+    async create(userRegister: User): Promise<string>{
+        const newUser = new this.userModel(userRegister);
+        
+        if(this.findUserByEmail(newUser.email)){
+            throw new HttpException("Mail already taken",HttpStatus.UNAUTHORIZED)
+        }
+        if(newUser.password.length==0){
+            throw new HttpException("Password is empty",HttpStatus.UNAUTHORIZED)
+        }
+        const round = Math.floor(Math.random()*25487595);
+        newUser.password = await bcrypt.hash(newUser.password,round);
+        let user = await newUser.save();
+        return user.id as string;
+    }
+
+
+    private async findUserByEmail(email: string):  Promise<User> {
+        return await this.userModel.findOne({email});
     }
 
 
