105fb287050716ac9ccd952841a66d03ba6338ea
Obey Senhaji <obey.senhaji@student.vinci.be>
2021-12-09 10:37:49 +0100

    fix package

--- pfe-backend/src/users/controllers/userController.ts
+++ pfe-backend/src/users/controllers/userController.ts
@@ -7,47 +7,41 @@ import { UserService } from '../services/userService';
 
 @Controller('user')
 export class UserController {
-  
-    constructor(private userService:UserService){}
-  
-@Get()
-async getUsers() {
-    return await this.userService.getUsers();
-  }
 
-@Get('/:id')
-getUserById(@Param('id') id: string){
-    return this.userService.getUser(id);
-}
+    constructor(private userService: UserService) { }
 
-@Put(':id')
-updateUser(@Param('id') id: String, @Body() userdto: User){
-    return 'update a user';
-}
-
-@Delete(':id')
-remove(@Param('id') id:String){
-    return 'remove a user';
-}
-@Post('login')
-async verifyUser(@Body() userDTO: UserDTO){
-    if(this.userService.verifyUser(userDTO)){
-        //TODO
+    @Get()
+    async getUsers() {
+        return await this.userService.getUsers();
     }
-}
 
-@Post('register')
-async createUser(@Body() userdto : User){
-    
-    return await this.userService.create(userdto);
+    @Get('/:id')
+    getUserById(@Param('id') id: string) {
+        return this.userService.getUser(id);
+    }
 
-}
+    @Put(':id')
+    updateUser(@Param('id') id: String, @Body() userdto: User) {
+        return 'update a user';
+    }
 
-@Post('login')
-async verify(@Body() userLoginDTO : LoginUserDto){
-    
-    return await this.userService.verify(userLoginDTO);
+    @Delete(':id')
+    remove(@Param('id') id: String) {
+        return 'remove a user';
+    }
 
-}
+    @Post('register')
+    async createUser(@Body() userdto: User) {
+
+        return await this.userService.create(userdto);
+
+    }
+
+    @Post('login')
+    async verify(@Body() userLoginDTO: LoginUserDto) {
+
+        return await this.userService.verify(userLoginDTO);
+
+    }
 
 }
