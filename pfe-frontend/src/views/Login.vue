f3db97ea32c9193f92d1cb2bf2011d4491f2ad6e
Julien Van Tongerloo <julien.vantongerloo@hotmail.com>
2021-12-09 10:27:27 +0100

    refactor - signup

---
+++ pfe-frontend/src/views/Login.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-card elevation="0">
+    <v-row style="height: 100vh">
+        <v-col cols=6>
+            <v-row justify=center>
+                <v-col
+                    cols="12"
+                    sm="10"
+                    md="8"
+                    lg="6"
+                    xl="4"
+                >
+                    <p class="pt-2 font-weight-thin">Pas encore de compte ? <a color=#158aaf href="/register">S'inscrire</a></p>
+                    <h1 class="pt-14 mb-5">Bon retour chez Vinci Market</h1>
+                    <p class="mb-10 font-weight-thin">Projet dans une démarche de développement durable visant à promouvoir le réemploi.</p>
+                    <v-form>
+                        <v-text-field
+                            label="Email vinci"
+                            outlined
+                            append-icon="mdi-account-outline"
+                        />
+                        <v-text-field
+                            label="Mot de passe"
+                            outlined
+                            append-icon="mdi-lock-outline"
+                        />
+                        <v-btn
+                            block
+                            large
+                            elevation="0"
+                            color=#158aaf
+                        >
+                            Se connecter
+                        </v-btn>
+                    </v-form>
+                </v-col>
+            </v-row>
+        </v-col>
+        <sign-up-canva/>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+    import SignUpCanva from '../components/SignUpCanva.vue'
+
+    export default {
+        name: 'Login',
+        components: {
+            SignUpCanva,
+        }
+    }
+</script>
