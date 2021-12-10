6b5c0c009fa6b45cb1538d7136e58d72b4b99bc8
dalil <dalil.rachik@student.vinci.be>
2021-12-10 09:41:27 +0100

    refactor - import loginuserdto

--- src/users/controllers/userController.ts
+++ src/users/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { LoginUserDto } from '../models/dto/loginUser.dto';
+import { LoginUserDto } from '../models/dto/loginuser.dto';
 import { User } from '../models/user.interface';
 import { UserService } from '../services/userService';
 
